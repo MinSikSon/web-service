840e2d3efa3d54a21216eaa080ab44ae29516830
minsikson <sonminsik@sonminsig-ui-MacBook-Pro.local>
2021-06-02 02:03:33 +0900

    1. 문제 추가. 2. 점수 계산 함수 추가가

--- hw_js_quiz/quiz.js
+++ hw_js_quiz/quiz.js
@@ -1,7 +1,7 @@
 class Quiz {
     static QUIZ_ORDER = 0;
-    static QUIZ_LIST = [];
     static QUIZ_NUMBER = 0;
+    static QUIZ_LIST = [];
     static QUIZ_SELECT = [];
     constructor(_title, _select, _answerIdx) {
         this.num = Quiz.QUIZ_NUMBER++;
@@ -25,6 +25,10 @@ class Quiz {
 
     get quizId() {
         return `quiz` + this.num;
+    }
+
+    setAnser(_idx) {
+        this.answerIdx = _idx;
     }
 }
 
@@ -76,7 +80,7 @@ document.addEventListener(`DOMContentLoaded`, function () {
     RegistQuiz();
 
     // display first Quiz
-    DisplayQuiz();
+    DisplayNextQuiz();
 });
 
 document.getElementById(`next-button`).onclick = function () {
@@ -99,7 +103,16 @@ document.getElementById(`next-button`).onclick = function () {
 
     ClearQuiz();
     if (Quiz.QUIZ_ORDER < Quiz.QUIZ_NUMBER) {
-        DisplayQuiz();
+        if (Quiz.QUIZ_ORDER + 1 === Quiz.QUIZ_NUMBER) {
+            let score = 0;
+            for (let i = 0; i < Quiz.QUIZ_NUMBER - 1; i++) {
+                if (Quiz.QUIZ_SELECT[i] === Quiz.QUIZ_LIST[i].answerIdx) {
+                    score++;
+                }
+            }
+            Quiz.QUIZ_LIST[Quiz.QUIZ_ORDER].setAnser(score);
+        }
+        DisplayNextQuiz();
     }
     else if (Quiz.QUIZ_ORDER === Quiz.QUIZ_NUMBER) {
         DisplayFinal();
@@ -107,20 +120,15 @@ document.getElementById(`next-button`).onclick = function () {
     }
 }
 
-function RegistQuiz() {
-    new Quiz(`1+1 ?`, [1, 2, 3, 4, 5], 1);
-    new Quiz(`내 나이는?`, [31, 32, 33, 34, 35], 2);
-    new Quiz(`매드몬스터 멤버 이름이 아닌 것은?`, [`탄`, `제이호`, `포켓몬스터`], 2);
-    console.log(Quiz.QUIZ_LIST);
-}
-
-
-function DisplayQuiz() {
-    document.getElementById(`quiz-id`).innerText = Quiz.QUIZ_ORDER;
+function DisplayNextQuiz() {
+    // quiz 출력
     document.getElementById(`quiz-title`).appendChild(Quiz.QUIZ_LIST[Quiz.QUIZ_ORDER].divTitle);
     for (let i = 0; i < Quiz.QUIZ_LIST[Quiz.QUIZ_ORDER].divRadio.length; i++) {
         document.getElementById(`quiz-select`).appendChild(Quiz.QUIZ_LIST[Quiz.QUIZ_ORDER].divRadio[i]);
     }
+
+    // footer 에 quiz 번호 출력
+    document.getElementById(`quiz-id`).innerText = `${Quiz.QUIZ_ORDER + 1} / ${Quiz.QUIZ_NUMBER}`;
 
     Quiz.QUIZ_ORDER++;
 }
@@ -128,18 +136,34 @@ function DisplayQuiz() {
 function DisplayFinal() {
     let divTitle = ElementCreator.CreateDiv({ innerText: `당신의 점수는...!` });
     document.getElementById(`quiz-title`).appendChild(divTitle);
+
+    // 점수계산
     let score = 0;
     for (let i = 0; i < Quiz.QUIZ_NUMBER; i++) {
         if (Quiz.QUIZ_SELECT[i] === Quiz.QUIZ_LIST[i].answerIdx) {
-            score += 10;
+            score++;
         }
     }
-    let divScore = ElementCreator.CreateDiv({ innerText: `점수: ${score} / ${Quiz.QUIZ_NUMBER * 10}` });
-    document.getElementById(`quiz-select`).appendChild(divScore);
 
+    let divScore = ElementCreator.CreateDiv({ innerText: `점수: ${rescaleScore(score)} / ${rescaleScore(Quiz.QUIZ_NUMBER)}` });
+    document.getElementById(`quiz-select`).appendChild(divScore);
+}
+
+function rescaleScore(_score) {
+    return (_score / Quiz.QUIZ_NUMBER) * 100;
 }
 
 function ClearQuiz() {
     ElementCreator.ClearInnerHtmlById(`quiz-title`);
     ElementCreator.ClearInnerHtmlById(`quiz-select`);
+}
+
+function RegistQuiz() {
+    // new Quiz(제목, 문제, 답);
+    new Quiz(`1 + 1 = ?`, [1, 2, 3, 4, 5], 1);
+    new Quiz(`내 나이는 몇 살 일까요??`, [`31살`, 32, 33, 34, 35], 2);
+    new Quiz(`매드몬스터 멤버가 아닌 것을 고르시오.`, [`탄`, `제이호`, `포켓몬스터`], 2);
+    new Quiz(`주식 연 수익이 24% 일 때, 자본이 2배가 될 때까지 얼마나 걸릴까요?`, [`3년`, `4년`, `5년`, `6년`], 0);
+    new Quiz(`지금까지 총 몇 문제 맞춘거 같나요?`, [0, 1, 2, 3, 4], 4);
+    // console.log(Quiz.QUIZ_LIST);
 }
